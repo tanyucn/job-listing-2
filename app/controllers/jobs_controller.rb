b00f25c77e21b53329ebd949fa929eabbc70a661
谭宇 <tanyucn@outlook.com>
2017-07-16 12:25:28 +0800

    翻修到 Helper之前，玩了会views/admin/jobs/index.html.erb，分开了Edit和Destroy

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,9 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+   #@jobs = Job.all.where(:is_hidden => false).order("create_ta DESC")
+   #@jobs = Job.all.where(:is_hidden => false)
   end
 
   def new
@@ -45,6 +47,6 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title,:description, :wage_upper_bound, :wage_lower_bound, :contact_email)
+    params.require(:job).permit(:title,:description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
