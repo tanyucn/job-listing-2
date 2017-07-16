294a02ec4d8174c35ac012e40a3382675eb3996e
谭宇 <tanyucn@outlook.com>
2017-07-16 22:46:32 +0800

    admin can see resume list(这一张admin新增查看三项数据时要注意添加内容在234表格位；简历设计要到step3完成才能看到)

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+      resources :resumes
     end
   end
 
@@ -18,5 +19,5 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  
+
 end
